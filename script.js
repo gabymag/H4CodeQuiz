b52dd0242518493260f4d1f8d0df1446e04d9194
gaby magnussen <gabymagnussen@gmail.com>
2020-01-20 21:03:11 -0600

    added more JS added more divs to html file

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 var startBtn = document.getElementById('startBtn');
 var questionList = document.getElementById('question-list');
 var introDisplay = document.getElementById('intro');
-var questionElement = document.getElementById('question')
-var answerChoices = document.getElementById('answer-selections')
+var questionElement = document.getElementById('question');
+var answerChoices = document.getElementById('answer-selections');
+var AnswerText = document.getElementById('AnswerText');
+var doneScreen = document.getElementById('doneScreen');
+var timerDisplay = document.getElementById('timer');
+var YourScore = document.getElementById('YourScore');
+var SubmitButton = document.getElementById('submitBtn');
+var inputField = document.getElementById('inputField');
+var savedInit = document.getElementById('savedinitials');
+var highScore = document.getElementById('high-score-screen');
+var goBack = document.getElementById('back-to-start');
+var QuestionNum = -1;
+var highScoreShow = document.getElementById('high-score-show');
+var time = 75;
+
+
+
+//questions array
 var questions = [
     {
         question: "It is recommended to end each Javascript statment with a",
@@ -33,21 +49,106 @@ var questions = [
 
 
 
-startBtn.addEventListener('click', startQuiz);//listening for click event on startBtn, run the startQuiz function
+startBtn.addEventListener('click', DoStuff);//listening for click event on startBtn, run the startQuiz function
+answerChoices.children[0].addEventListener('click', DoStuff);
+answerChoices.children[1].addEventListener('click', DoStuff);
+answerChoices.children[2].addEventListener('click', DoStuff);
+answerChoices.children[3].addEventListener('click', DoStuff);
+SubmitButton.addEventListener('click', DoStuff);
+goBack.addEventListener('click', DoStuff);
 
+function DoStuff(){
+  if(QuestionNum == -1){
+    QuestionNum++;
+    startQuiz();
+  }
+  else if(QuestionNum >-1 && QuestionNum < questions.length){
+        //var ans = event.srcElement.innerText;
+        //AnswerText.innerHTML ="<hr>" + ans;
+        //
+        var childnum = Array.prototype.indexOf.call(answerChoices.children, event.srcElement) + 1; //within event. find source element(.srcElement)returns the index + 1
+        // AnswerText.innerHTML ="<hr>" + childnum;
+        AnswerText.classList.remove('hide');
+        if(childnum == questions[QuestionNum].answer) {//if childnum is equal to the the right index choice
+          AnswerText.innerHTML = "<hr>" +  "Correct!";//tell user correct
+        }
+        else{
+          AnswerText.innerHTML = "<hr>" +  "Wrong!";//otherwrise tell user its 
+          //wrong
+          decrementTimer(15);
+        }
+        clearTimeout(hideElement);//cancels out the setTimeout function
+        setTimeout(hideElement,1000);//after 1 sec hide the text 
+        QuestionNum++;
+        if(QuestionNum == questions.length  /*3*/ ){//once it reaches all questions, them hide the questions
+          questionList.classList.add('hide');
+          //display Input Box
+          clearInterval(a); // stops the timer
+          doneScreen.classList.remove('hide');
+          YourScore.innerText = "Your score is " + time;
+          QuestionNum++;
+        }else{//next question
+          viewQuestion(QuestionNum);
+        }
+      }  
+      else if(QuestionNum == questions.length + 1){//display high scores
+        doneScreen.classList.add('hide');
+        highScore.classList.remove('hide');
+        highScoreShow.innerText = localStorage.getItem("user") + ": " + localStorage.getItem("score");
+        highScoreShow.innerText += "\n" + savedInit.value + ": " + time;
+        localStorage.setItem("user", savedInit.value);
+        localStorage.setItem("score",time);
+        QuestionNum++;
+      }
+      else{//restart code
+          startBtn.classList.remove('hide');
+          highScore.classList.add('hide');
+          introDisplay.classList.remove('hide');
+          QuestionNum = -1;
+          time = 75;
+          timerDisplay.innerText = "Time: 75";
+      }
+}
 
 function startQuiz(){//when quiz is started the start button needs to hide and the container that holds the quiz questions needs to show up
-startBtn.classList.add('hide');
-introDisplay.classList.add('hide');
-questionList.classList.remove('hide');
+    startBtn.classList.add('hide');
+    introDisplay.classList.add('hide');
+    questionList.classList.remove('hide');
+    viewQuestion(QuestionNum);
+    a = setInterval(decrementTimer,1000,1)
 }
 
 
-  
-// function viewQuestion(){
+  //calling the questions and choices, take the inner text set it equal to [i]th element in the questions array, and the same for the answer buttons, calling the child buttons
+ function viewQuestion(i){
+   questionElement.innerText = questions[i].question;
+   answerChoices.children[0].innerText = questions[i].choice1;
+   answerChoices.children[1].innerText = questions[i].choice2;
+   answerChoices.children[2].innerText = questions[i].choice3;
+   answerChoices.children[3].innerText = questions[i].choice4;
 
-// }
-// function chooseAnswer{
-    
-// }
+ }
 
+ function chooseAnswer(answer,j){
+    if(i == questions[QuestionNum].answer) {
+        AnswerText.innerHTML = "Correct!";
+    }
+    else{
+        AnswerText.innerHTML = "Incorrect!";
+    }
+    QuestionNum++;
+    viewQuestion(QuestionNum);
+ }
+
+
+function hideElement(){//function for the wrong or correct text at the bottom
+  AnswerText.classList.add('hide');//hides the wrong or correct response
+}
+function decrementTimer(i){ // score function
+  time=time - i;
+  if(time < 0){
+    time = 0;
+  }
+  timerDisplay.innerText = "Time: " + time
+
+}
